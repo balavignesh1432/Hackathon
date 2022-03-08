dc127b5e21a58b29ca0ccebb3aca0a07f858c01c
balavignesh1432 <balavignesh1432@gmail.com>
2022-03-08 11:20:05 +0530

    Milestone 1A

--- Milestone_1A.py
+++ Milestone_1A.py
@@ -1,20 +1,26 @@
 import yaml
 import datetime
+import time
 import csv
 from PIL import Image
-
-def timeFunc(parent, secs):
+logs = []
+def timeFunc(parent, times):
     currTime = datetime.datetime.now()
-    string = str(currTime) + ';' + parent + ' Executing TimeFunction(' + secs + ')'
+    string = str(currTime) + ';' + parent + ' Executing TimeFunction(' + times['FunctionInput'] + ', ' + times['ExecutionTime'] + ')'
     print(string)
+    logs.append(string)
+    time.sleep(int(times['ExecutionTime']))
 
 def performTask(parent, task):
     if task['Function'] == "TimeFunction":
         currTime = datetime.datetime.now()
         string = str(currTime) + ';' + parent + ' Entry'
         print(string)
-        timeFunc(parent, task['Inputs']['ExecutionTime'])
+        logs.append(str(currTime) + ';' + parent + ' Entry')
+        timeFunc(parent, task['Inputs'])
+        currTime = datetime.datetime.now()
         print(str(currTime) + ';' + parent + ' Exit')
+        logs.append(str(currTime) + ';' + parent + ' Exit')
 
 def performFlow(parent, values):
     for k in values.keys():
@@ -23,13 +29,19 @@ def performFlow(parent, values):
             if parent == '':
                 currTime = datetime.datetime.now()
                 print(str(currTime) + ';' + parent + k + ' Entry')
+                logs.append(str(currTime) + ';' + parent + k + ' Entry')
                 performFlow(parent + k, values[k]['Activities'])
+                currTime = datetime.datetime.now()
                 print(str(currTime) + ';' + parent + k + ' Exit')
+                logs.append(str(currTime) + ';' + parent + k + ' Exit')
             else:
                 currTime = datetime.datetime.now()
                 print(str(currTime) + ';' + parent + '.' + k + ' Entry')
+                logs.append(str(currTime) + ';' + parent + '.' + k + ' Entry')
                 performFlow(parent + '.' + k, values[k]['Activities'])
+                currTime = datetime.datetime.now()
                 print(str(currTime) + ';' + parent + '.' + k + ' Exit')
+                logs.append(str(currTime) + ';' + parent + '.' + k + ' Exit')
 
         if values[k]['Type'] == 'Task':
             performTask(parent + '.' + k, values[k])
@@ -41,11 +53,9 @@ if __name__ == '__main__':
     # Yaml Parsed Dictionary
     # print(YamlParsed)
     yamlFile.close()
+    logFile = open("Milestone1A_Log.txt", 'w')
     performFlow("", yamlParsed)
-
-    # csvFile = open('Sample.csv', 'w', encoding='UTF-8', newline="")
-    # csvwriter = csv.writer(csvFile)
-    #
-    # # Write Entities as a List into csv
-    # csvwriter.writerows([])
-    # csvFile.close()
+    for log in logs:
+        logFile.write(log)
+        logFile.write('\n')
+    logFile.close()
